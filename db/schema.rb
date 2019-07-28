fc4c2d71f23208f6576a73b8668eb826067883cc
kevin lang <langkevina@gmail.com>
2019-07-28 16:54:22 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_164427) do
-
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.integer "release_date"
-    t.string "lead"
-    t.string "hometown"
-    t.boolean "in_theaters"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
